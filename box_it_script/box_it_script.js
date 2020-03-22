af48eef35c156039b04480f90b14e002b80bb67b
helgakern <helgakern@gmail.com>
2020-03-22 12:35:00 -0700

    completed homework

--- box_it_script/box_it_script.js
+++ box_it_script/box_it_script.js
@@ -1,71 +1,59 @@
-// const arg = process.argv[2];
+// [Assignment] Box It Script - Week 1
 
-// This function takes a number as argument and returns that number of horizontal bars as a string.    
+// This function takes a number as an argument and returns that number of horizontal bars as a string.    
 function drawLine(num){
     return '─'.repeat(num);
 };
-// console.log(drawLine(4));
 
-// This function takes a number as argument and draw a top border.
+// This function takes a number as an argument and draw a top border.
 function drawTopBorder(num){
-    return '┌'+drawLine((num - 2))+'┐';
- // return '┌'+'─'.repeat(num - 2)+'┐';
+    return '┌' + drawLine(num) + '┐';
 };
-// console.log(drawTopBorder(4));
 
-// This function takes a number as argument and draw a middle border.
+// This function takes a number as an argument and draw a middle border.
 function drawMidleBorder(num){
-    return '├'+drawLine((num - 2))+'┤';
-    // return '├'+'─'.repeat(num - 2)+'┤';
+    return '├' + drawLine(num) + '┤';
 };
-// console.log(drawMidleBorder(4));
 
-// This function takes a number as argument and draw a bottom border.
+// This function takes a number as an argument and draw a bottom border.
 function drawBottomBorder(num){
-    return '└'+drawLine((num - 2))+'┘';
-    // return '└'+'─'.repeat(num - 2)+'┘';
+    return '└' + drawLine(num) + '┘';
 };
-// console.log(drawBottomBorder(4));
 
-// This function takes a string as argument and surrounds it with vertical lines and returns it.
-function drawBarsAround(str){
-    return '│'+ str +'│';
+// This function takes a string as an arguments and surrounds it with vertical lines and returns it.
+function drawBarsAround(str, num){
+    return '│' + str + ' '.repeat(num - str.length) + '│';
 };
-// console.log(drawBarsAround('Helga'));
 
-// This function that takes an array of strings and returns a string where each is in a single column table.
-// To add "new lines" to a string, use the \n special character. In a string, \n characters will 
-//display as new lines  when logged with console.log(...).
+/* This function that takes an array of strings and returns a string where each is in a single column table.
+To add "new lines" to a string, use the \n special character. In a string, \n characters will 
+display as new lines  when logged with console.log(...).
+*/
 
 function boxIt(arr){
-    return drawTopBorder(arr[0].length) +`\n`+ drawBarsAround(arr[0]) +`\n`+ drawBottomBorder(arr[0].length);
-    // return drawTopBorder(arr[1].length) +`\n`+ drawBarsAround(arr[1]) +`\n`+ drawBottomBorder(arr[1].length);
+    let longest = arr[0];
+    let result = '';
 
-    // return drawTopBorder(arr.length)+`\n`+ drawBarsAround(arr.length) +`\n`+ drawMidleBorder() +`\n`+ drawBarsAround() +`\n`+ drawBottomBorder();
+    for (let str of arr){
+        if (str.length > longest.length){
+            longest = str;
+        }
+    }
+    const num = longest.length;
 
+    for (let i = 0; i < arr.length; i++){
+        if (arr.length === 1){
+            result += `${drawTopBorder(num)}\n${drawBarsAround(arr[i], num)}\n${drawBottomBorder(num)}`;
+        } else if (i === 0){
+            result += `${drawTopBorder(num)}\n${drawBarsAround(arr[i], num)}\n`;
+        } else if (i > 0 && i < arr.length - 1){
+            result += `${drawMidleBorder(num)}\n${drawBarsAround(arr[i], num)}\n${drawMidleBorder(num)}\n`;
+        } else {
+            result += `${drawBarsAround(arr[i], num)}\n${drawBottomBorder(num)}`;
+        }
+    }
+    return result;
 };
-// console.log(boxIt("Jon Snow"));
-console.log(boxIt(['Jon Snow', 'Cersei Lannister']));
 
-
-
-// const array = str.split()
-
-// function boxIt([strOne, strTwo]){
-//     return drawTopBorder()+`\n`+ drawBarsAround() +`\n`+ drawMidleBorder() +`\n`+ drawBarsAround() +`\n`+ drawBottomBorder();
-// };
-
-function boxIt(arr){
-    let nameOne = arr[0];
-    let nameTwo = arr[1];
-
-    return drawTopBorder(arr[0].length) +`\n`+ drawBarsAround(arr[0]) +`\n`+ drawBottomBorder(arr[0].length);
-    // return drawTopBorder(arr[1].length) +`\n`+ drawBarsAround(arr[1]) +`\n`+ drawBottomBorder(arr[1].length);
-
-    // return drawTopBorder(arr.length)+`\n`+ drawBarsAround(arr.length) +`\n`+ drawMidleBorder() +`\n`+ drawBarsAround() +`\n`+ drawBottomBorder();
-
-};
-// console.log(boxIt("Jon Snow"));
-console.log(boxIt(['Jon Snow', 'Cersei Lannister']));
-
-
+console.log(boxIt(['Jon Snow']));
+console.log(boxIt(['Jon Snow', 'Cersei Lannister', 'Daenerys Targaryen']));
