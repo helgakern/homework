02cffe1c733eb2601eaafbc0a402b7e82a0e2358
helgakern <helgakern@gmail.com>
2020-04-01 17:42:18 -0700

    created todo cli file

---
+++ todo_CLI/todoCLI.js
@@ -0,0 +1,7 @@
+/* Todo CLI – Week 3
+Write an interactive CLI todo list application using Node's 
+readline and fs modules. The following describes what each 
+action does. It would be best to implement each action as 
+its own function.
+*/
+
