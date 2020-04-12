0b1157013ba9e006197985a5e6426a7c1d19ba61
helgakern <helgakern@gmail.com>
2020-04-11 19:14:05 -0700

    created index.js and json files, and installed dependencies

---
+++ team_picker/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("welcome");
+});
+
+const DOMAIN = "localhost";
+const PORT = "3000";
+app.listen(PORT, DOMAIN, () => {
+  console.log(`Server is listening on ${DOMAIN}:${PORT}`);
+});
